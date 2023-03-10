c22f19c5ac0e8fab6a386fc201d07f18fb50af46
Gian Carlo Marino Polo <Gian.Marino@UPDS.edu.bo>
2023-03-10 13:34:18 -0400

    List generated dynamically in sidenav component

--- src/app/components/shared/side-bar/side-bar.component.ts
+++ src/app/components/shared/side-bar/side-bar.component.ts
@@ -2,6 +2,16 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { MainService } from 'src/app/services/main.service';
 
+
+//? Temporaly MenuInterface
+//? May change due backend response, Menu is created dynamically
+interface MenuItem{
+  routerLink: string;
+  title: string;
+  icon: string
+}
+
+
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
@@ -9,7 +19,7 @@ import { MainService } from 'src/app/services/main.service';
 })
 export class SideBarComponent implements OnInit {
 
-  @Input() menus: any[] = [];
+  @Input() menus: MenuItem[] = [];
   @Output() aplicarCambios: EventEmitter<void> = new EventEmitter<void>()
   @Output() cambiarModoEvent: EventEmitter<void> = new EventEmitter<void>()
 
@@ -29,6 +39,66 @@ export class SideBarComponent implements OnInit {
       this.tema = 'dark';
       this.modoOscuro = true;
     }
+
+    //? Overwriting default value of menus
+    this.menus = [
+      {
+        routerLink: "/",
+        title: "Estatutos y Reglamentos",
+        icon: "bi bi-journal-bookmark-fill"
+      },
+      {
+        routerLink: "/",
+        title: "Solicitud de licencia",
+        icon: "bi bi-file-medical-fill"
+      },
+      {
+        routerLink: "/",
+        title: "Lista de Utiles Escolares",
+        icon: "bi bi-list-task"
+      },
+      {
+        routerLink: "/",
+        title: "Horario de Clases",
+        icon: "bi bi-alarm-fill"
+      },
+      {
+        routerLink: "/",
+        title: "Citaciones",
+        icon: "bi bi-send-check-fill"
+      },
+      {
+        routerLink: "/",
+        title: "Planificación curricular",
+        icon: "bi bi-file-ruled-fill"
+      },
+      {
+        routerLink: "/",
+        title: "Pagos",
+        icon: "bi bi-file-ruled-fill"
+      },
+      {
+        routerLink: "/",
+        title: "Postulacion",
+        icon: "bi bi-file-ruled-fill"
+      },
+      {
+        routerLink: "/",
+        title: "Lista de postulacion",
+        icon: "bi bi-file-ruled-fill"
+      },
+      {
+        routerLink: "/",
+        title: "Lista de familiares",
+        icon: "bi bi-file-ruled-fill"
+      },
+      {
+        routerLink: "/",
+        title: "Familiar",
+        icon: "bi bi-file-ruled-fill"
+      }
+    ];
+
   }
   
   toggleOff(){
